8aa3b12ee0426056e39986840cf8bc75f868a2e2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-03-01 12:02:07 -0300

    Add cover image and other stuff

--- src/components/ShareButtons/index.tsx
+++ src/components/ShareButtons/index.tsx
@@ -15,7 +15,8 @@ const ShareButtons = () => (
     </a>
 
     <a
-      href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fnextjs-willianjusten.com.br%2F&text=Quero%20aprender%20NextJS%20na%20pr%E1tica%20com%20@Willian_justen%20e%20@guilhermelouro%21"
+      href={`
+https://twitter.com/intent/tweet?text=Quero%20aprender%20NextJS%20na%20pr%C3%A1tica%20com%20%40Willian_justen%20e%20%40guilhermelouro%20!&url=https%3A%2F%2Fnextjs.willianjusten.com.br%2F`}
       target="_blank"
       rel="noreferrer"
       aria-label="Twitter"
