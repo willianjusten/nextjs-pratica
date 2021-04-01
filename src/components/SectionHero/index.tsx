ab3985116a393ebf724601a43ed3577ee8b498b4
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-01 14:51:31 -0300

    Add PROMOABRIL21 coupon

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -21,7 +21,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOMAR21"
+              href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOABRIL21"
               onClick={onClick}
               wide
             >
