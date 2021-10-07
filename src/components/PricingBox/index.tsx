67cbfb0b86979005dbca1500c70f56244909fb47
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-10-07 12:00:27 -0300

    Add PROMOOUT21 coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -35,7 +35,7 @@ const PricingBox = () => (
       </S.BenefitsItem>
     </S.BenefitsList>
 
-    <Button href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOSET21" onClick={onClick} withPrice>
+    <Button href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOOUT21" onClick={onClick} withPrice>
       <p>Comprar o curso</p>
       <div>
         <S.ButtonFullPrice>R$54.99</S.ButtonFullPrice>
--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -21,7 +21,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOSET21"
+              href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOOUT21"
               onClick={onClick}
               wide
             >
