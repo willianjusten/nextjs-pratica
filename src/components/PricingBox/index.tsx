aa3c6b65ef6afa019e1c3e80b457b9f643f851b9
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-06-03 19:20:04 -0300

    Update price

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -35,15 +35,13 @@ const PricingBox = () => (
       </S.BenefitsItem>
     </S.BenefitsList>
 
-    <Button href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOMAI22" onClick={onClick} withPrice>
+    <Button href="https://www.udemy.com/course/aprenda-nextjs-na-pratica/?couponCode=PROMOJUN22" onClick={onClick} withPrice>
       <p>Comprar o curso</p>
       <div>
-        <S.ButtonFullPrice>R$54.99</S.ButtonFullPrice>
-        <S.ButtonDiscountPrice>R$39.90</S.ButtonDiscountPrice>
+        <S.ButtonFullPrice>R$79.99</S.ButtonFullPrice>
+        <S.ButtonDiscountPrice>R$54.90</S.ButtonDiscountPrice>
       </div>
     </Button>
-
-    <S.SmallText><strong>*</strong> nos 5 primeiros dias</S.SmallText>
   </S.Box>
 )
 
